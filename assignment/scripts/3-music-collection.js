051523835135c653c73358216fefbe779c9fedc3
Natalie McMann <nmcmann110@gmail.com>
2022-11-21 13:10:35 -0600

    created addToCollection function and tested it

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,13 +2,28 @@ console.log('***** Music Collection *****')
 
 //- Create a variable `collection` that starts as an empty array.
 let collection = [];
+console.log('Record collection is empty:', collection);
 
 //- Add a function named `addToCollection`. This function should:
 //- Take in the album's `title`, `artist`, `yearPublished` as input parameters
 //- Create a new object having the above properties
 //- Add the new object to the end of the `collection` array
 //- Return the newly created object
+function addToCollection(title, artist, yearPublished) {
+    let album = {
+        albumTitle: title,
+        albumArtist: artist,
+        albumYear: yearPublished
+    }
+    collection.push(album);
+    return album;
+}
 
+//test addToCollection
+console.log('Add Rumours by Fleetwood Mac to the collection:', addToCollection('Rumours', 'Fleetwood Mac', 1977));
+//add some more music albums
+console.log('Add The Black Parade by My Chemical Romance to the collection:', addToCollection('The Black Parade', 'My Chemical Romance', 2006));
+console.log('Add Lonerism by Tame Impala to the collection:', addToCollection('Lonerism', 'Tame Impala', 2012));
 
 //- Add a function named `showCollection`. This function should:
 //- Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
