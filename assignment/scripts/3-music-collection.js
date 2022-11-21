e25607b483d3413a4b1cf59aaa0709b0ed6c3586
Natalie McMann <nmcmann110@gmail.com>
2022-11-21 10:52:21 -0600

    first read through, added required tasks to js file

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,29 @@
 console.log('***** Music Collection *****')
+
+//- Create a variable `collection` that starts as an empty array.
+let collection = [];
+
+//- Add a function named `addToCollection`. This function should:
+//- Take in the album's `title`, `artist`, `yearPublished` as input parameters
+//- Create a new object having the above properties
+//- Add the new object to the end of the `collection` array
+//- Return the newly created object
+
+
+//- Add a function named `showCollection`. This function should:
+//- Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+//- Console.log the number of items in the array.
+//- Loop over the array and console.log each album's information formatted like: 
+//`TITLE by ARTIST, published in YEAR`.
+
+//- Test the `showCollection` function.
+
+//- Add a function named `findByArtist`. This function should:
+//  - Take in `artist` (a string) parameter
+//  - Create an array to hold any results, empty to start
+//  - Loop through the `collection` and add any objects with a matching artist to the array.
+//  - Return the array with the matching results. If no results are found, return an empty array.
+
+//- Test the `findByArtist` function.
+
+//Stretch Goals
